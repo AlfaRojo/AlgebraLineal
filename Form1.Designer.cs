93ccedd24adab2ab82d5a5976bbe6b7d64aae9dc
KEVIN ORTIZ RAMIREZ <kortiz@correo.url.edu.gt>
2020-07-10 23:01:16 -0600

    Proyecto Algebra Lineal

---
+++ Form1.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace PhotoCookie
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.BImportar = new System.Windows.Forms.Button();
+            this.PBOriginal = new System.Windows.Forms.PictureBox();
+            this.PBFinal = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.Picture = new System.Windows.Forms.PictureBox();
+            this.BBlancoNegro = new System.Windows.Forms.Button();
+            this.Tipos = new System.Windows.Forms.ComboBox();
+            this.BAplicar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.PBOriginal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PBFinal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Picture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BImportar
+            // 
+            this.BImportar.BackColor = System.Drawing.Color.Transparent;
+            this.BImportar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BImportar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BImportar.ForeColor = System.Drawing.Color.White;
+            this.BImportar.Location = new System.Drawing.Point(12, 537);
+            this.BImportar.Name = "BImportar";
+            this.BImportar.Size = new System.Drawing.Size(117, 68);
+            this.BImportar.TabIndex = 0;
+            this.BImportar.Text = "Importar";
+            this.BImportar.UseVisualStyleBackColor = false;
+            this.BImportar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // PBOriginal
+            // 
+            this.PBOriginal.BackColor = System.Drawing.Color.Transparent;
+            this.PBOriginal.Location = new System.Drawing.Point(10, 10);
+            this.PBOriginal.Name = "PBOriginal";
+            this.PBOriginal.Size = new System.Drawing.Size(298, 426);
+            this.PBOriginal.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PBOriginal.TabIndex = 1;
+            this.PBOriginal.TabStop = false;
+            // 
+            // PBFinal
+            // 
+            this.PBFinal.BackColor = System.Drawing.Color.Transparent;
+            this.PBFinal.Location = new System.Drawing.Point(376, 10);
+            this.PBFinal.Name = "PBFinal";
+            this.PBFinal.Size = new System.Drawing.Size(298, 426);
+            this.PBFinal.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PBFinal.TabIndex = 2;
+            this.PBFinal.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(494, 508);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(28, 22);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "0";
+            this.textBox1.Visible = false;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(528, 508);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(28, 22);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "0";
+            this.textBox2.Visible = false;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(562, 508);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(28, 22);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.Text = "0";
+            this.textBox3.Visible = false;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(562, 537);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(28, 22);
+            this.textBox4.TabIndex = 9;
+            this.textBox4.Text = "0";
+            this.textBox4.Visible = false;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(528, 537);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(28, 22);
+            this.textBox5.TabIndex = 8;
+            this.textBox5.Text = "0";
+            this.textBox5.Visible = false;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(494, 537);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(28, 22);
+            this.textBox6.TabIndex = 7;
+            this.textBox6.Text = "0";
+            this.textBox6.Visible = false;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(562, 565);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(28, 22);
+            this.textBox7.TabIndex = 12;
+            this.textBox7.Text = "0";
+            this.textBox7.Visible = false;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(528, 565);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(28, 22);
+            this.textBox8.TabIndex = 11;
+            this.textBox8.Text = "0";
+            this.textBox8.Visible = false;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(494, 565);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(28, 22);
+            this.textBox9.TabIndex = 10;
+            this.textBox9.Text = "0";
+            this.textBox9.Visible = false;
+            // 
+            // Picture
+            // 
+            this.Picture.BackColor = System.Drawing.Color.Transparent;
+            this.Picture.Location = new System.Drawing.Point(741, 12);
+            this.Picture.Name = "Picture";
+            this.Picture.Size = new System.Drawing.Size(298, 426);
+            this.Picture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Picture.TabIndex = 13;
+            this.Picture.TabStop = false;
+            // 
+            // BBlancoNegro
+            // 
+            this.BBlancoNegro.BackColor = System.Drawing.Color.Transparent;
+            this.BBlancoNegro.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BBlancoNegro.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BBlancoNegro.ForeColor = System.Drawing.Color.White;
+            this.BBlancoNegro.Location = new System.Drawing.Point(466, 442);
+            this.BBlancoNegro.Name = "BBlancoNegro";
+            this.BBlancoNegro.Size = new System.Drawing.Size(146, 33);
+            this.BBlancoNegro.TabIndex = 14;
+            this.BBlancoNegro.Text = "Blanco-Negro";
+            this.BBlancoNegro.UseVisualStyleBackColor = false;
+            this.BBlancoNegro.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Tipos
+            // 
+            this.Tipos.FormattingEnabled = true;
+            this.Tipos.Items.AddRange(new object[] {
+            "Difuminado",
+            "Realzar",
+            "Sobel Inferior",
+            "Sobel Superior",
+            "Sobel Izquierdo",
+            "Sobel Derecho",
+            "Contorno",
+            "Afilar",
+            "Original",
+            "Personalizado"});
+            this.Tipos.Location = new System.Drawing.Point(744, 444);
+            this.Tipos.Name = "Tipos";
+            this.Tipos.Size = new System.Drawing.Size(121, 24);
+            this.Tipos.TabIndex = 16;
+            this.Tipos.SelectedIndexChanged += new System.EventHandler(this.Tipos_SelectedIndexChanged);
+            // 
+            // BAplicar
+            // 
+            this.BAplicar.BackColor = System.Drawing.Color.Transparent;
+            this.BAplicar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BAplicar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BAplicar.ForeColor = System.Drawing.Color.White;
+            this.BAplicar.Location = new System.Drawing.Point(893, 444);
+            this.BAplicar.Name = "BAplicar";
+            this.BAplicar.Size = new System.Drawing.Size(146, 33);
+            this.BAplicar.TabIndex = 17;
+            this.BAplicar.Text = "Aplicar";
+            this.BAplicar.UseVisualStyleBackColor = false;
+            this.BAplicar.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label1.Font = new System.Drawing.Font("Palatino Linotype", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label1.Location = new System.Drawing.Point(744, 480);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(295, 125);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Función";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1051, 617);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BAplicar);
+            this.Controls.Add(this.Tipos);
+            this.Controls.Add(this.BBlancoNegro);
+            this.Controls.Add(this.Picture);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.PBFinal);
+            this.Controls.Add(this.PBOriginal);
+            this.Controls.Add(this.BImportar);
+            this.ForeColor = System.Drawing.Color.White;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Algebra Lineal";
+            ((System.ComponentModel.ISupportInitialize)(this.PBOriginal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PBFinal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Picture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BImportar;
+        private System.Windows.Forms.PictureBox PBOriginal;
+        private System.Windows.Forms.PictureBox PBFinal;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.PictureBox Picture;
+        private System.Windows.Forms.Button BBlancoNegro;
+        private System.Windows.Forms.ComboBox Tipos;
+        private System.Windows.Forms.Button BAplicar;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,321 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace PhotoCookie
+{
+    public partial class Form1 : Form
+    {
+        string rutaArchivo = string.Empty;
+        int wI = 0;
+        int hI = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private static readonly ImageConverter _imageConverter = new ImageConverter();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog busquedaArchivo = new OpenFileDialog())
+            {
+                busquedaArchivo.InitialDirectory = "c:\\";
+                busquedaArchivo.Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png";
+                busquedaArchivo.FilterIndex = 2;
+                busquedaArchivo.RestoreDirectory = true;
+                if (busquedaArchivo.ShowDialog() == DialogResult.OK)
+                {
+                    PBOriginal.InitialImage = null;
+                    PBFinal.InitialImage = null;
+                    Picture.InitialImage = null;
+                    rutaArchivo = busquedaArchivo.FileName;
+                    byte[] imgdata = File.ReadAllBytes(rutaArchivo);
+                    var Inicial = GetImageFromByteArray(imgdata);
+                    wI = Inicial.Width;
+                    hI = Inicial.Height;
+                    PBOriginal.Image = Inicial;
+                }
+                else
+                {
+                    return;
+                }
+            }
+        }
+        /// <summary>
+        /// Convierte la imagen a escala de grises
+        /// </summary>
+        /// <param name="bm"></param>
+        /// <returns></returns>
+        public static Bitmap MedianFiltering(Bitmap bm)
+        {
+            List<byte> termsList = new List<byte>();
+            byte[,] image = new byte[bm.Width, bm.Height];
+            for (int i = 0; i < bm.Width; i++)
+            {
+                for (int j = 0; j < bm.Height; j++)
+                {
+                    var c = bm.GetPixel(i, j);
+                    byte gray = (byte)(.333 * c.R + .333 * c.G + .333 * c.B);
+                    image[i, j] = gray;
+                }
+            }
+            for (int i = 0; i <= bm.Width - 3; i++)
+                for (int j = 0; j <= bm.Height - 3; j++)
+                {
+                    for (int x = i; x <= i + 2; x++)
+                        for (int y = j; y <= j + 2; y++)
+                        {
+                            termsList.Add(image[x, y]);
+                        }
+                    byte[] terms = termsList.ToArray();
+                    termsList.Clear();
+                    Array.Sort<byte>(terms);
+                    Array.Reverse(terms);
+                    byte color = terms[4];
+                    bm.SetPixel(i + 1, j + 1, Color.FromArgb(color, color, color));
+                }
+            return bm;
+        }
+        /// <summary>
+        /// Obtiene Bitmap de la imagen
+        /// </summary>
+        /// <param name="arregloByte"></param>
+        /// <returns></returns>
+        public static Bitmap GetImageFromByteArray(byte[] arregloByte)
+        {
+            Bitmap bm = (Bitmap)_imageConverter.ConvertFrom(arregloByte);
+            if (bm != null && (bm.HorizontalResolution != (int)bm.HorizontalResolution ||
+                               bm.VerticalResolution != (int)bm.VerticalResolution))
+            {
+                bm.SetResolution((int)(bm.HorizontalResolution + 0.5f),
+                                 (int)(bm.VerticalResolution + 0.5f));
+            }
+            return bm;
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (PBOriginal.Image == null)
+            {
+                MessageBox.Show("No se ha cargado ninguna imagen.", "Algebra Lineal", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            Bitmap matrizBlancoNegro = new Bitmap(PBOriginal.Image);
+            MedianFiltering(matrizBlancoNegro);
+            double[,] ValuesDouble = new double[matrizBlancoNegro.Width, matrizBlancoNegro.Height];
+            for (int i = 0; i < matrizBlancoNegro.Width - 1; i++)
+            {
+                for (int j = 0; j < matrizBlancoNegro.Height - 1; j++)
+                {
+                    ValuesDouble[i, j] = matrizBlancoNegro.GetPixel(i, j).R;
+                }
+            }
+            var bmp = new Bitmap(matrizBlancoNegro);
+            PBFinal.Image = matrizBlancoNegro;
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double a11 = 0; double a12 = 0; double a13 = 0;
+            double a21 = 0; double a22 = 0; double a23 = 0;
+            double a31 = 0; double a32 = 0; double a33 = 0;
+            if (PBOriginal.Image == null)
+            {
+                MessageBox.Show("No se ha cargado ninguna imagen.", "Algebra Lineal", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            label1.Text = Tipos.Text;
+
+            switch (Tipos.Text)//Establecer valores a Matriz a multiplicar
+            {
+                case "Difuminado":
+                    a11 = 0.0625; a12 = 0.125; a13 = 0.0625;
+                    a21 = 0.125; a22 = 0.25; a23 = 0.125;
+                    a31 = 0.0625; a32 = 0.125; a33 = 0.0625;
+                    break;
+                case "Realzar":
+                    a11 = -2; a12 = -1; a13 = 0;
+                    a21 = -1; a22 = 1; a23 = 1;
+                    a31 = 0; a32 = 1; a33 = 2;
+                    break;
+                case "Sobel Inferior":
+                    a11 = -1; a12 = -2; a13 = -1;
+                    a21 = 0; a22 = 0; a23 = 0;
+                    a31 = 1; a32 = 2; a33 = 1;
+                    break;
+                case "Sobel Superior":
+                    a11 = 1; a12 = 2; a13 = 1;
+                    a21 = 0; a22 = 0; a23 = 0;
+                    a31 = -1; a32 = -2; a33 = -1;
+                    break;
+                case "Sobel Izquierdo":
+                    a11 = 1; a12 = 0; a13 = -1;
+                    a21 = 2; a22 = 0; a23 = -2;
+                    a31 = 1; a32 = 0; a33 = -1;
+                    break;
+                case "Sobel Derecho":
+                    a11 = -1; a12 = 0; a13 = 1;
+                    a21 = -2; a22 = 0; a23 = 2;
+                    a31 = -1; a32 = 0; a33 = 1;
+                    break;
+                case "Contorno":
+                    a11 = -1; a12 = -1; a13 = -1;
+                    a21 = -1; a22 = 8; a23 = -1;
+                    a31 = -1; a32 = -1; a33 = -1;
+                    break;
+                case "Afilar":
+                    a11 = 0; a12 = -1; a13 = 0;
+                    a21 = -1; a22 = 5; a23 = -1;
+                    a31 = 0; a32 = -1; a33 = 0;
+                    break;
+                case "Original":
+                    a11 = 0; a12 = 0; a13 = 0;
+                    a21 = 0; a22 = 1; a23 = 0;
+                    a31 = 0; a32 = 0; a33 = 0;
+                    break;
+                case "Personalizado":
+
+                    a11 = Convert.ToDouble(textBox1.Text);
+                    a11 = int.Parse(textBox1.Text);
+                    a12 = Convert.ToDouble(textBox1.Text);
+                    a12 = int.Parse(textBox2.Text);
+                    a13 = Convert.ToDouble(textBox1.Text);
+                    a13 = int.Parse(textBox3.Text);
+
+                    a21 = Convert.ToDouble(textBox1.Text);
+                    a21 = int.Parse(textBox4.Text);
+                    a22 = Convert.ToDouble(textBox1.Text);
+                    a22 = int.Parse(textBox5.Text);
+                    a23 = Convert.ToDouble(textBox1.Text);
+                    a23 = int.Parse(textBox6.Text);
+
+                    a31 = Convert.ToDouble(textBox1.Text);
+                    a31 = int.Parse(textBox7.Text);
+                    a32 = Convert.ToDouble(textBox1.Text);
+                    a32 = int.Parse(textBox8.Text);
+                    a33 = Convert.ToDouble(textBox1.Text);
+                    a33 = int.Parse(textBox9.Text);
+                    break;
+                case "":
+                    MessageBox.Show("Porfavor seleccione una opción.");
+                    break;
+                default:
+                    break;
+            }
+
+            byte[] imgdata = File.ReadAllBytes(rutaArchivo);
+            PBOriginal.Image = GetImageFromByteArray(imgdata);
+            Bitmap toBlackWhite = new Bitmap(PBFinal.Image);
+
+            int Ancho = wI;
+            int Alto = hI;
+            var Filtro = new Bitmap(Ancho, Alto);
+            int[,] M = new int[Ancho, Alto];
+            int[,] R = new int[Ancho, Alto];
+            for (int l = 0; l < Ancho; l++)
+            {
+                for (int p = 0; p < Alto; p++)
+                {
+                    M[l, p] = toBlackWhite.GetPixel(l, p).R;
+                }
+            }
+            //Proceso de multiplicación
+            for (int i = 0; i < Ancho; i++)
+            {
+                for (int j = 0; j < Alto; j++)
+                {
+                    if (i == 0)
+                    {
+                        if (j == 0 || j == Alto - 1 || ((j > 0) && (j < Alto - 1)))
+                        {
+                            R[i, j] = M[i, j];
+                        }
+                    }
+                    if (i == Ancho - 1)
+                    {
+                        if (j == 0 || j == Alto - 1 || ((j > 0) && (j < Alto - 1)))
+                        {
+                            R[i, j] = M[i, j];
+                        }
+                    }
+                    if (j == 0)
+                    {
+                        if ((i > 0) && (i < Ancho - 1))
+                        {
+                            R[i, j] = M[i, j];
+                        }
+                    }
+                    if (j == Alto - 1)
+                    {
+                        if ((i > 0) && (i < Ancho - 1))
+                        {
+                            R[i, j] = M[i, j];
+                        }
+                    }
+                    if ((i > 0) && (j > 0) && (i < Ancho - 1) && j < Alto - 1)
+                    {
+                        R[i, j] = (int)
+                             (M[i - 1, j - 1] * a11
+                            + M[i - 1, j] * a12
+                            + M[i - 1, j + 1] * a13
+                            + M[i, j - 1] * a21
+                            + M[i, j] * a22
+                            + M[i, j + 1] * a23
+                            + M[i + 1, j - 1] * a31
+                            + M[i + 1, j] * a32
+                            + M[i + 1, j + 1] * a33
+                            );
+                    }
+                    //Comprobar limites de los colores
+                    if (R[i, j] > 255)
+                    {
+                        R[i, j] = 255;
+                    }
+                    if (R[i, j] < 0)
+                    {
+                        R[i, j] = 0;
+                    }
+                    int img = R[i, j];
+                    var nuevo = Color.FromArgb(img, img, img);
+                    Filtro.SetPixel(i, j, nuevo);
+                }
+            }
+            Picture.Image = Filtro;
+        }
+        private void Tipos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (Tipos.Text == "Personalizado")
+            {
+                textBox1.Visible = true;
+                textBox2.Visible = true;
+                textBox3.Visible = true;
+                textBox4.Visible = true;
+                textBox5.Visible = true;
+                textBox6.Visible = true;
+                textBox7.Visible = true;
+                textBox8.Visible = true;
+                textBox9.Visible = true;
+            }
+            else
+            {
+                textBox1.Visible = false;
+                textBox2.Visible = false;
+                textBox3.Visible = false;
+                textBox4.Visible = false;
+                textBox5.Visible = false;
+                textBox6.Visible = false;
+                textBox7.Visible = false;
+                textBox8.Visible = false;
+                textBox9.Visible = false;
+            }
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (Regex.IsMatch(textBox1.Text, "[^0-9]"))
+            {
+                MessageBox.Show("Porfavor ingrese números solamente.");
+                textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - 1);
+            }
+        }
+    }
+}
